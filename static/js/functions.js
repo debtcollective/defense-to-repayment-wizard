828ef84778fb0ae1decf59cc6bc2ceee6d9a75b1
Karissa McKelvey <krmckelv@gmail.com>
2015-10-13 12:05:02 -0700

    prevent return key from prematurely submitting

--- static/js/functions.js
+++ static/js/functions.js
@@ -389,4 +389,12 @@ var section_page = 1;
 	       return results[1] || 0;
 	    }
 	}
+
+  function stopRKey(evt) {
+      var evt = (evt) ? evt : ((event) ? event : null);
+        var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+          if ((evt.keyCode == 13) && (node.type=="text"))  {return false;}
+  }
+
+  document.onkeypress = stopRKey;
 })(window.jQuery);
